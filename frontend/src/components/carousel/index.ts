ee1e6d3443c0eb48a12706eefbec7d8c842661e5
Debra <rizki.andrian45@gmail.com>
2025-11-03 19:19:32 +0700

    Fix

--- frontend/src/components/carousel/index.ts
+++
@@ -1,4 +0,0 @@
-export { CurvedCarousel } from './CurvedCarousel'
-export { CarouselCard } from './CarouselCard'
-export { CarouselProgress } from './CarouselProgress'
-export { FrameSlider } from './FrameSlider'
